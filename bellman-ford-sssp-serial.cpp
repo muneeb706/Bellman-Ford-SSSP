ed6734a3e88fbf0628f872512bcd0a06852c2a5a
Muneeb Shahid <muneeb706@gmail.com>
2024-01-20 18:48:40 -0800

    docs(bellmean-ford-sssp-serial): Docstrings added #1

--- bellman-ford-sssp-serial.cpp
+++ bellman-ford-sssp-serial.cpp
@@ -2,7 +2,7 @@
 #include <sstream>	   // For string stream operations
 #include <fstream>	   // For file handling operations
 #include <curl/curl.h> // For file download operations
-#include <cstdio> // For popen() used in extract_tar_file()
+#include <cstdio>	   // For popen() used in extract_tar_file()
 
 using namespace std;
 
@@ -20,6 +20,120 @@ struct Edge
 	int weight;
 };
 
+/**
+ * @brief Writes data to a stream.
+ *
+ * This function is a callback used by libcurl to write data to a stream. It is typically
+ * used when downloading a file. The data to be written is provided by libcurl and the
+ * stream to which it should be written is provided as an argument.
+ *
+ * @param ptr A pointer to the data to be written.
+ * @param size The size of each unit of data to be written.
+ * @param nmemb The number of units of data to be written.
+ * @param stream The stream to which the data should be written.
+ * @return The number of bytes actually written.
+ */
+size_t write_data(void *ptr, size_t size, size_t nmemb, FILE *stream)
+{
+	size_t written = fwrite(ptr, size, nmemb, stream);
+	return written;
+}
+
+/**
+ * @brief Downloads a file from a given URL and saves it to a specified path.
+ *
+ * This function uses libcurl to download a file. If the file already exists at the
+ * specified path, the download is skipped. If the file does not exist, it is created
+ * and the content from the URL is written to it. If the download fails for any reason,
+ * an error message is printed to the console.
+ *
+ * @param url The URL of the file to download.
+ * @param file_path The path where the downloaded file should be saved.
+ */
+void download_file(const char *url, const char *file_path)
+{
+	std::ifstream file(file_path);
+	if (file.good())
+	{
+		// File already exists
+		std::cout << "Tar File already exists, skipping download.\n";
+		return;
+	}
+
+	CURL *curl;
+	FILE *fp;
+	CURLcode res;
+
+	curl = curl_easy_init();
+	if (curl)
+	{
+		fp = fopen(file_path, "wb");
+		if (fp == NULL)
+		{
+			std::cout << "Could not open file for writing: " << file_path << std::endl;
+			return;
+		}
+		curl_easy_setopt(curl, CURLOPT_URL, url);
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+		curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+		curl_easy_setopt(curl, CURLOPT_FAILONERROR, 1L);
+		res = curl_easy_perform(curl);
+		if (res != CURLE_OK)
+		{
+			std::cout << "curl_easy_perform() failed: " << curl_easy_strerror(res) << std::endl;
+		}
+		/* always cleanup */
+		curl_easy_cleanup(curl);
+		fclose(fp);
+	}
+}
+
+/**
+ * @brief Extracts a tar file to a specified directory.
+ *
+ * This function uses the libarchive library to extract a tar file. The tar file and the
+ * directory to which it should be extracted are provided as arguments. If the extraction
+ * fails for any reason, an error message is printed to the console.
+ *
+ * @param tar_file_path The path to the tar file to be extracted.
+ * @param extract_dir_path The path to the directory where the tar file should be extracted.
+ */
+void extract_tar_file(const char *file_path)
+{
+	std::string command = "tar -xzf ";
+	command += file_path;
+
+	FILE *pipe = popen(command.c_str(), "r");
+	if (!pipe)
+	{
+		return;
+	}
+	pclose(pipe);
+
+	// Remove the file after extracting it
+	if (std::remove(file_path) != 0)
+	{
+		std::perror("Error deleting file");
+	}
+	else
+	{
+		std::cout << "Tar File successfully deleted.\n";
+	}
+}
+
+/**
+ * @brief Reads a graph from a file and returns a pointer to the graph.
+ *
+ * This function reads a graph from a file whose path is provided as an argument.
+ * The graph is represented as an adjacency matrix. The first line of the file should
+ * contain three integers: the number of nodes, the number of edges, and the weight of the edge.
+ * Each subsequent line should contain three integers: the source node, the destination node,
+ * and the weight of the edge between them.
+ *
+ * @param input_graph_path The path to the file containing the graph data.
+ * @return A pointer to the graph read from the file.
+ */
 struct Graph *readGraph(string input_graph_path)
 {
 	struct Graph *graph = (struct Graph *)malloc(sizeof(struct Graph));
@@ -37,7 +151,8 @@ struct Graph *readGraph(string input_graph_path)
 		while (getline(myfile, line))
 		{
 			// Skip comments
-			if (!line.empty() && line[0] == '%') {
+			if (!line.empty() && line[0] == '%')
+			{
 				continue;
 			}
 			// value of parameter will be stored in this variable.
@@ -47,32 +162,20 @@ struct Graph *readGraph(string input_graph_path)
 			// stringstream class check1
 			stringstream check1(line);
 			int param_no = 0;
-			// variables to store three parameters in each line separately
-			int param_1, param_2, param_3;
+			// variable to store three parameters in a line
+			int params[3];
 			// Tokenizing each line to get parameter values w.r.t. space ' '
 			while (getline(check1, token, ' ') && param_no < 3)
 			{
-				if (param_no == 0)
-				{
-					param_1 = stoi(token, &sz);
-				}
-				else if (param_no == 1)
-				{
-					param_2 = stoi(token, &sz);
-				}
-				else if (param_no == 2)
-				{
-					param_3 = stoi(token, &sz);
-				}
-				param_no++;
+				params[param_no++] = stoi(token, &sz);
 			}
 
 			// reading parameters in first line
 			if (line_no == 0)
 			{
 
-				int no_of_nodes = param_1;
-				int no_of_edges = param_3;
+				int no_of_nodes = params[0];
+				int no_of_edges = params[2];
 				graph->no_of_nodes = no_of_nodes;
 				graph->no_of_edges = no_of_edges;
 
@@ -82,9 +185,9 @@ struct Graph *readGraph(string input_graph_path)
 			{
 
 				// for every other line
-				int source = param_1;
-				int destination = param_2;
-				int weight = param_3;
+				int source = params[0];
+				int destination = params[1];
+				int weight = params[2];
 				graph->edge[line_no - 1].source = source;
 				graph->edge[line_no - 1].destination = destination;
 				graph->edge[line_no - 1].weight = weight;
@@ -98,64 +201,21 @@ struct Graph *readGraph(string input_graph_path)
 	return graph;
 }
 
-size_t write_data(void *ptr, size_t size, size_t nmemb, FILE *stream)
-{
-	size_t written = fwrite(ptr, size, nmemb, stream);
-	return written;
-}
-
-void download_file(const char* url, const char* file_path) {
-    std::ifstream file(file_path);
-    if (file.good()) {
-        // File already exists
-        std::cout << "File already exists, skipping download.\n";
-        return;
-    }
-
-    CURL* curl;
-    FILE* fp;
-    CURLcode res;
-
-    curl = curl_easy_init();
-    if (curl) {
-        fp = fopen(file_path, "wb");
-        if(fp == NULL) {
-            std::cout << "Could not open file for writing: " << file_path << std::endl;
-            return;
-        }
-        curl_easy_setopt(curl, CURLOPT_URL, url);
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
-        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
-        curl_easy_setopt(curl, CURLOPT_FAILONERROR, 1L);
-        res = curl_easy_perform(curl);
-        if(res != CURLE_OK) {
-            std::cout << "curl_easy_perform() failed: " << curl_easy_strerror(res) << std::endl;
-        }
-        /* always cleanup */
-        curl_easy_cleanup(curl);
-        fclose(fp);
-    }
-}
-
-void extract_tar_file(const char* file_path) {
-    std::string command = "tar -xzf ";
-    command += file_path;
-
-    FILE* pipe = popen(command.c_str(), "r");
-    if (!pipe) {
-        return;
-    }
-    pclose(pipe);
-
-    // Remove the file after extracting it
-    if (std::remove(file_path) != 0) {
-        std::perror("Error deleting file");
-    } else {
-        std::cout << "File successfully deleted.\n";
-    }
-}
-
+/**
+ * @brief Runs the Bellman-Ford algorithm to find the shortest path from a source node to all other nodes in a graph.
+ *
+ * This function implements the Bellman-Ford algorithm for finding the shortest path from a source node to all other nodes in a graph.
+ * The graph and the source node ID are provided as arguments. The graph is represented as an adjacency list.
+ * The function initializes the distance to all nodes as infinity, except for the source node which is set to zero.
+ * It then relaxes the edges of the graph in a loop that runs for (number of nodes - 1) times.
+ * If in any iteration no edge is relaxed, it breaks out of the loop.
+ * Finally, it checks for the presence of negative weight cycles in the graph.
+ * If such a cycle is found, it prints a message to the console.
+ * The function does not return a value; instead, it prints the shortest distance from the source node to node 100.
+ *
+ * @param graph A pointer to the graph on which the algorithm should be run.
+ * @param source_node_id The ID of the source node from which the shortest paths should be found.
+ */
 void runBellmanFordSSSP(struct Graph *graph, int source_node_id)
 {
 	int no_of_nodes = graph->no_of_nodes;
@@ -164,10 +224,11 @@ void runBellmanFordSSSP(struct Graph *graph, int source_node_id)
 	// distance of node 10 will be at index 10.
 	int node_distances[no_of_nodes];
 
-	// setting initial distances
+	// setting initial distances to infinity
 	for (int i = 0; i < no_of_nodes; i++)
 		node_distances[i] = numeric_limits<int>::max() - 100;
 
+	// setting the distance of the source node to zero
 	node_distances[source_node_id] = 0;
 
 	for (int i = 0; i < no_of_nodes - 1; i++)
@@ -214,7 +275,6 @@ void runBellmanFordSSSP(struct Graph *graph, int source_node_id)
 
 int main()
 {
-	int source_node_id = 0;
 
 	download_file("https://suitesparse-collection-website.herokuapp.com/MM/SNAP/higgs-twitter.tar.gz",
 				  "higgs-twitter.tar.gz");
@@ -222,11 +282,16 @@ int main()
 	// update this variable to provide path to input data file i.e sparse matrix
 	string input_graph_path = "higgs-twitter/higgs-twitter.mtx";
 
+	int source_node_id = 0;
+
 	struct Graph *graph = readGraph(input_graph_path);
 
 	auto t1 = chrono::high_resolution_clock::now();
+
 	runBellmanFordSSSP(graph, source_node_id);
+
 	auto t2 = chrono::high_resolution_clock::now();
+
 	auto duration_1 = chrono::duration_cast<chrono::microseconds>(t2 - t1).count();
 	cout << "Execution time (microseconds): " << duration_1 << endl;
 
